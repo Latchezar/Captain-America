42eba0d661d3121521bf8da72ce5549caa207610
Latchezar Nikolov <latcho.nikolov@gmail.com>
2018-06-27 16:04:15 +0300

    test changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        int num = 1;
+        for (int i = 0; i < num; i++) {
+            System.out.println(i);
+        }
     }
 }
