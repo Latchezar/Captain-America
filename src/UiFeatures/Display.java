5f9d807543deda6c1eabc75a112a305da937f727
Latchezar Nikolov <latcho.nikolov@gmail.com>
2018-06-29 21:06:34 +0300

    added some ui features
    added display and game class to start the game itself

---
+++ src/UiFeatures/Display.java
@@ -0,0 +1,36 @@
+package UiFeatures;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display {
+    private JFrame displayFrame;
+    private Canvas displayCanvas;
+
+    private int width, height;
+    private String title;
+    public Display(String title, int width, int height){
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createDisplay();
+    }
+
+    private void createDisplay(){
+        displayFrame = new JFrame(title);
+        displayFrame.setSize(width, height);
+        displayFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        displayFrame.setResizable(false);
+        displayFrame.setLocationRelativeTo(null);
+        displayFrame.setVisible(true);
+
+        displayCanvas = new Canvas();
+        displayCanvas.setPreferredSize(new Dimension(width, height));
+        displayCanvas.setMaximumSize(new Dimension(width, height));
+        displayCanvas.setMinimumSize(new Dimension(width, height));
+
+        displayFrame.add(displayCanvas);
+        displayFrame.pack();
+    }
+}
