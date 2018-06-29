50c9e0b117dc238b192a009b6059bca2e48d17fc
Latchezar Nikolov <latcho.nikolov@gmail.com>
2018-06-29 21:21:00 +0300

    Some more UI
    created the game frame and game loop

--- src/UiFeatures/Game.java
+++ src/UiFeatures/Game.java
@@ -4,30 +4,58 @@ public class Game implements Runnable{
 
     private Display display;
     public int width, height;
+    public String title;
 
+    private boolean isRunning = false;
     private Thread thread;
 
     public Game(String title, int width, int height){
         this.width = width;
         this.height = height;
+        this.title = title;
 
-        display = new Display(title, width, height);
     }
 
     private void init(){
-        
+        display = new Display(title, width, height);
+
+    }
+
+    private void update(){
+
+    }
+
+    private void render(){
+
     }
 
     public void run(){
         init();
+
+        while (isRunning){
+
+            update();
+            render();
+
+            stop();
+        }
+
     }
 
     public synchronized void start() {
+        if (isRunning){
+            return;
+        }
+        isRunning = true;
         thread = new Thread(this);
         thread.start();
     }
 
     public synchronized void stop() {
+        if (!isRunning){
+            return;
+        }
+        isRunning = false;
         try {
             thread.join();
         } catch (InterruptedException e) {
